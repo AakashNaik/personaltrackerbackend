0a1fe7e9f98ce48f9ee0a7c54bfb2c159d663aa6
aashi <aashi@padrumonkey.in>
2022-12-14 18:05:10 +0530

    added rest api to fetch data

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-//const api = require('./api');
- 
-const port=3000;
-const app=express();
-  
-app.listen(port, function() {
-    console.log("Server is listening at port:" + port);
-});
- 
-// Parses the text as url encoded data
-app.use(bodyParser.urlencoded({extended: true}));
- 
-// Parses the text as json
-app.use(bodyParser.json());
- 
-//app.use('/api', api);
+//require('dotenv').config();
+const express = require('express');
+//const mongoose = require('mongoose');
+//const mongoString = process.env.DATABASE_URL;
+
+const cors =require('cors');
+const whitelist = ["http://localhost:3000"];
+
+const corsOptions = {
+
+  origin: function (origin, callback) {
+
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+
+      callback(null, true)
+
+    } else {
+
+      callback(new Error("Not allowed by CORS"))
+
+    }
+
+  },
+
+  credentials: true,
+
+}
+
+//mongoose.connect(mongoString);
+//const database = mongoose.connection;
+const routes = require('./routes/route');
+
+// database.on('error', (error) => {
+//     console.log(error)
+// })
+
+// database.once('connected', () => {
+//     console.log('Database Connected');
+// })
+const app = express();
+app.use(cors(corsOptions))
+
+app.use(express.json());
+app.use('/api', routes)
+
+app.listen(4001, () => {
+    console.log(`Server Started at ${4001}`)
+})
---
+++ routes/route.js
@@ -0,0 +1,52 @@
+const express = require('express');
+//const Model = require('../models/model');
+const router = express.Router()
+
+
+
+//Post Method
+router.post('/post', async (req, res) => {
+    // const data = new Model({
+    //     name: req.body.name,
+    //     age: req.body.age
+    // })
+    // console.log('error1');
+    // try {
+    //     const dataToSave = await data.save();
+    //     res.status(200).json(dataToSave)
+    // }
+    // catch (error) {
+    //     console.log('error');
+    //     res.status(400).json({message: error.message})
+    // }
+    res.send({'output':'post'})
+})
+
+//Get all Method
+router.get('/getAll', async(req, res) => {
+    // try{
+    //     const data = await Model.find();
+    //     res.json(data)
+    // }
+    // catch(error){
+    //     res.status(500).json({message: error.message})
+    // }
+    res.send({'output':'get All'})
+})
+
+//Get by ID Method
+router.get('/getOne/:id', (req, res) => {
+    res.send('Get by ID API')
+})
+
+//Update by ID Method
+router.patch('/update/:id', (req, res) => {
+    res.send('Update by ID API')
+})
+
+//Delete by ID Method
+router.delete('/delete/:id', (req, res) => {
+    res.send('Delete by ID API')
+})
+
+module.exports = router;
